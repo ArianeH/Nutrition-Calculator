78b231188302dadd1405936b59b01c48493fc5c6
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2017-10-10 10:56:09 +0200

    Added tags which display selected food types

--- app/assets/javascripts/pages/home.js
+++ app/assets/javascripts/pages/home.js
@@ -10,6 +10,11 @@
     $('.apple-select').click(function() {
       $('.apple-nutrition').addClass('visible');
       $('.milk-nutrition').removeClass('visible');
+      $('.container-selected-food').append('<div class="selected-food"></div>');
+      $('.selected-food').html($(this).text() + ' <button class="close">&times;</button>');
+      $('.close').click(function(){
+        $('.selected-food').addClass('invisible');
+      });
     });
     $('.apple-100-select').click(function() {
       $('.apple-nutrition').addClass('visible');
@@ -18,14 +23,20 @@
     $('.milk-select').click(function() {
       $('.milk-nutrition').addClass('visible');
       $('.apple-nutrition').removeClass('visible');
+      $('.container-selected-food').append('<div class="selected-food"></div>');
+      $('.selected-food').html($(this).text() + ' <button class="close">&times;</button>');
+      $('.close').click(function(){
+        $('.selected-food').addClass('invisible');
+      });
     });
     $('.milk-100-select').click(function() {
       $('.milk-nutrition').addClass('visible');
       $('.apple-nutrition').removeClass('visible');
     });
-  });
 
-  $(".dropdown-menu li button").click(function(){
-    $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
-    $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+    $(".dropdown-menu li button").click(function(){
+      $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
+      $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+    });
+
   });
