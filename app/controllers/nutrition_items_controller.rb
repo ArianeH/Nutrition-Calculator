b6e3dd80af2e96b2a2f662506ecefd251d671772
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2017-10-14 22:54:30 +0200

    Working on remote connection to partial view

--- app/controllers/nutrition_items_controller.rb
+++ app/controllers/nutrition_items_controller.rb
@@ -1,9 +1,14 @@
 class NutritionItemsController < ApplicationController
 
   def calculate
-    sum_calories = 0
+    @sum_calories = 24
+    puts "herro"
+    # sum += NutritionItem.calories
 
-
+    respond_to do |format|
+      format.html { render(:text => "not implemented") }
+      format.js  # <-- will render `app/views/profiles/show.js.erb`
+    end
   end
 
 end
