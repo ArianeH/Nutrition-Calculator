de941d0799ae256b752b896668a19c6a3b5ebe73
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2017-10-17 09:55:27 +0200

    Progress in adding possibility to sum selected nutrition items

--- app/controllers/nutrition_items_controller.rb
+++ app/controllers/nutrition_items_controller.rb
@@ -2,6 +2,11 @@ class NutritionItemsController < ApplicationController
 
   def calculate
     @selected = NutritionItem.where(category: params[:item]).first
+    @sum_selected = params[:sum_selected].to_i
+
+    if @sum_selected
+      @sum_selected += @selected.calories
+    end
 
     respond_to do |format|
       format.js  # <-- will render `app/views/nutrition_items/calculate.js.erb`
