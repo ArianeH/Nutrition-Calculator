df0d24da488a006b5b310b0348586bfe46fe6596
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2017-11-08 17:30:11 +0100

    Added landing page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # get 'pages/home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'pages#home'
+  root to: 'pages#landing'
   get '/calculate', to: 'nutrition_items#calculate'
-
+  get '/nutrition-calculation', to: 'pages#home'
 end
