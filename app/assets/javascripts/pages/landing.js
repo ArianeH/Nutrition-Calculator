a7d11def894c60e97953759430eacd284250705f
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2017-11-09 19:09:04 +0100

    Added scrolling function to the intro-explanation when button is clicked

--- app/assets/javascripts/pages/landing.js
+++ app/assets/javascripts/pages/landing.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
   $('#more-info-btn').click(function() {
-    $('.intro-explanation').addClass('visible');
+    $('.intro-explanation-wrapper').attr("id","visible");
+    $('#more-info-btn').attr("id","invisible");
+    $('html, body').animate({
+        scrollTop: $("#intro-explanation").offset().top
+    }, 2000);
   });
 });
