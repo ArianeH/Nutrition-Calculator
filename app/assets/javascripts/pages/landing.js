c342f80c93ec22212238c7af3052237e73c120ba
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2017-11-09 19:37:38 +0100

    Added image as new icon to scrolling button and adjusted scrolling effect

--- app/assets/javascripts/pages/landing.js
+++ app/assets/javascripts/pages/landing.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('#more-info-btn').click(function() {
-    $('.intro-explanation-wrapper').attr("id","visible");
     $('#more-info-btn').attr("id","invisible");
+    $('.intro-explanation-wrapper').attr("id","visible");
     $('html, body').animate({
         scrollTop: $("#intro-explanation").offset().top
     }, 2000);
