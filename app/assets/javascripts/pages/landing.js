3b808ac4cf28bea776948672643e4caf983e9e30
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2017-11-14 11:06:35 +0100

    Removed animation on How it works part on the landing page

--- app/assets/javascripts/pages/landing.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $('#more-info-btn').click(function() {
-    $('#more-info-btn').attr("id","invisible");
-    $('.intro-explanation-wrapper').attr("id","visible");
-    $('html, body').animate({
-        scrollTop: $("#intro-explanation").offset().top
-    }, 2000);
-  });
-});
