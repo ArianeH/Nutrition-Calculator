029cf0cb0aa5a8fe7a6ff5844dc22afd8219cfc5
Anton Holgersson <holgersson31@hotmail.com>
2017-12-06 21:05:08 +0100

    Added nutrients to link and summing them in nutrition controller.

--- app/controllers/nutrition_items_controller.rb
+++ app/controllers/nutrition_items_controller.rb
@@ -2,11 +2,31 @@ class NutritionItemsController < ApplicationController
 
   def calculate
     @selected = NutritionItem.where(code: params[:selected_code]).first
-    @sum_selected = params[:sum_selected].to_i
 
-    if @sum_selected
-      @sum_selected += @selected.calories
-    end
+    calories = params[:sum_calories]
+    protein = params[:sum_protein]
+    sugar = params[:sum_sugar]
+    fiber = params[:sum_fiber]
+    fat = params[:sum_fat]
+    saturated = params[:sum_saturated]
+    monounsaturated = params[:sum_monounsaturated]
+    polyunsaturated = params[:sum_polyunsaturated]
+    omega_3 = params[:sum_omega_3]
+    omega_6 = params[:sum_omega_6]
+    trans_fat = params[:sum_trans_fat]
+
+    selected.calories += calories
+    selected.protein += protein
+    selected.carbs += carbs
+    selected.sugar += sugar
+    selected.fiber += fiber
+    selected.fat += fat
+    selected.saturated += saturated
+    selected.monounsaturated += monounsaturated
+    selected.polyunsaturated += polyunsaturated
+    selected.omega_3 += omega_3
+    selected.omega_6 += omega_6
+    selected.trans_fat += trans_fat
 
     respond_to do |format|
       format.js  # <-- will render `app/views/nutrition_items/calculate.js.erb`
