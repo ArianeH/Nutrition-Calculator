17b542e585e345bf64a59fc1018d7ccf4e6d7093
Anton Holgersson <holgersson31@hotmail.com>
2017-12-07 21:31:22 +0100

    Add summing of all nutrients.

--- app/controllers/nutrition_items_controller.rb
+++ app/controllers/nutrition_items_controller.rb
@@ -2,31 +2,33 @@ class NutritionItemsController < ApplicationController
 
   def calculate
     @selected = NutritionItem.where(code: params[:selected_code]).first
+    @selected_sum = @selected.dup
 
-    calories = params[:sum_calories]
-    protein = params[:sum_protein]
-    sugar = params[:sum_sugar]
-    fiber = params[:sum_fiber]
-    fat = params[:sum_fat]
-    saturated = params[:sum_saturated]
-    monounsaturated = params[:sum_monounsaturated]
-    polyunsaturated = params[:sum_polyunsaturated]
-    omega_3 = params[:sum_omega_3]
-    omega_6 = params[:sum_omega_6]
-    trans_fat = params[:sum_trans_fat]
+    calories = params[:sum_calories].to_i
+    protein = params[:sum_protein].to_f
+    carbs = params[:sum_carbs].to_f
+    sugar = params[:sum_sugar].to_f
+    fiber = params[:sum_fiber].to_f
+    fat = params[:sum_fat].to_f
+    saturated = params[:sum_saturated].to_f
+    monounsaturated = params[:sum_monounsaturated].to_f
+    polyunsaturated = params[:sum_polyunsaturated].to_f
+    omega_3 = params[:sum_omega_3].to_f
+    omega_6 = params[:sum_omega_6].to_f
+    trans_fat = params[:sum_trans_fat].to_f
 
-    selected.calories += calories
-    selected.protein += protein
-    selected.carbs += carbs
-    selected.sugar += sugar
-    selected.fiber += fiber
-    selected.fat += fat
-    selected.saturated += saturated
-    selected.monounsaturated += monounsaturated
-    selected.polyunsaturated += polyunsaturated
-    selected.omega_3 += omega_3
-    selected.omega_6 += omega_6
-    selected.trans_fat += trans_fat
+    @selected_sum.calories += calories
+    @selected_sum.protein += protein
+    @selected_sum.carbs += carbs
+    @selected_sum.sugar += sugar
+    @selected_sum.fiber += fiber
+    @selected_sum.fat += fat
+    @selected_sum.saturated += saturated
+    @selected_sum.monounsaturated += monounsaturated
+    @selected_sum.polyunsaturated += polyunsaturated
+    @selected_sum.omega_3 += omega_3
+    @selected_sum.omega_6 += omega_6
+    @selected_sum.trans_fat += trans_fat
 
     respond_to do |format|
       format.js  # <-- will render `app/views/nutrition_items/calculate.js.erb`
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,16 +2,16 @@ class PagesController < ApplicationController
   def home
     @nutrition_items = NutritionItem.all
 
-    @sum_calories: 0
-    @sum_protein: 0
-    @sum_sugar: 0
-    @sum_fiber: 0
-    @sum_fat: 0
-    @sum_saturated: 0
-    @sum_monounsaturated: 0
-    @sum_polyunsaturated: 0
-    @sum_omega_3: 0
-    @sum_omega_6: 0
-    @sum_trans_fat: 0
+    @sum_calories = 0
+    @sum_protein = 0
+    @sum_sugar = 0
+    @sum_fiber = 0
+    @sum_fat = 0
+    @sum_saturated = 0
+    @sum_monounsaturated = 0
+    @sum_polyunsaturated = 0
+    @sum_omega_3 = 0
+    @sum_omega_6 = 0
+    @sum_trans_fat = 0
   end
 end
